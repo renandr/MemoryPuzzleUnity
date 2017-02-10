8d2751cf7dbe99ea19b599061eebc5528968ee8f
renan <renandr@gmail.com>
2017-02-10 23:59:21 +0100

    version 1

--- Assets/Source/Main.cs
+++ Assets/Source/Main.cs
@@ -1,13 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour {
     public static Camera Camera;
-
     public Camera MainCamera;
 
     public Texture2D source;
     public GameObject spritesRoot;
     public Piece templatePiece;
+    public static List<List<Piece>> slots;
+
+    private static int iTotal = 4;
+    private static int jTotal = 4;
+    
+    private static int pieceWidth;
+    private static int pieceHeight;
+
+
+    private static Piece selectedPiece;
+
+    public static Piece SelectedPiece{
+        set{
+            if (value) {
+                followerPiece.transform.SetAsLastSibling();
+                value.transform.SetAsLastSibling();
+            }
+            followerPiece.SetActive(value);
+            selectedPiece = value;
+        }
+        get{
+            return selectedPiece;
+        }
+    }
+
+    private static GameObject followerPiece;
 
     void Start () {
         Camera = MainCamera;
@@ -15,31 +43,76 @@ public class Main : MonoBehaviour {
     }
 
     private void Croppy(){
-        int iTotal = 4;
-        int jTotal = 4;
-        int pieceWidth = source.width / iTotal;
-        int pieceHeight = source.height / jTotal;
+        slots = new List<List<Piece>>();
+        pieceWidth = source.width / iTotal;
+        pieceHeight = source.height / jTotal;
 
         Debug.Log(source.width + "x" + source.height);
         Debug.Log(pieceWidth + "x" + pieceHeight);
 
+        followerPiece = new GameObject();
+        followerPiece.gameObject.transform.SetParent(spritesRoot.transform, false);
+        followerPiece.name = "Follower";
+        Texture2D texture = new Texture2D(pieceWidth, pieceHeight);
+        texture.SetPixel(0, 0, Color.white);
+        texture.Apply();
+        var image = followerPiece.AddComponent<Image>();
+        image.sprite = Sprite.Create(texture, new Rect(0,0,pieceWidth, pieceHeight),new Vector2(0,0), 1);
+        followerPiece.GetComponent<RectTransform>().sizeDelta = new Vector2(pieceWidth, pieceHeight);
+        //followerPiece.GetComponent<RectTransform>().pivot = new Vector2(0,0);
+
         for (int i = 0; i < iTotal; i++)
         {
+            var slotCollumn = new List<Piece>(jTotal*2);
+            slots.Add(slotCollumn);
             for (int j = 0; j < jTotal; j++)
             {
                 var n = Instantiate(templatePiece);
+                slotCollumn.Add(n);
                 n.SetPieceImage(Sprite.Create(source, new Rect(i * pieceWidth, j * pieceHeight, pieceWidth, pieceHeight),
-                    new Vector2(0, 0)), i, j);
+                    new Vector2(0, 0), 1), i, j);
                 n.name = "Piece " + i +" "+ j;
                 n.gameObject.transform.SetParent(spritesRoot.transform, false);
-                //n.transform.position = new Vector3(i * pieceWidth, j * pieceHeight, 0);
+                
             }
         }
+        for (int i = 0; i < iTotal; i++) {
+            var slotCollumn = new List<Piece>(jTotal * 2);
+            slots.Add(slotCollumn);
+        }
     }
-
-	
-	
+    
 	void Update () {
-		
+        if (selectedPiece)
+        {
+            followerPiece.transform.localPosition = GetGridSnappedPosition(selectedPiece.gameObject.transform.localPosition);
+        }
 	}
+
+    internal static void SetValidPosition(Piece piece)
+    {
+        slots[piece.currentGridX][piece.currentGridY] = null;
+        int newPosX = (int)Math.Round(piece.transform.localPosition.x / pieceWidth);
+        int newPosY = (int)Math.Round(piece.transform.localPosition.y / pieceHeight);
+        
+        if (slots[newPosX][newPosY] == null) {
+            piece.transform.localPosition = GetGridSnappedPosition(piece.gameObject.transform.localPosition);
+        } else {
+            piece.transform.localPosition = GetGridSnappedPosition(GetPositionForSlot(piece.currentGridX, piece.currentGridY));
+        }
+        slots[newPosX][newPosY] = piece;
+    }
+    
+    private static Vector3 GetPositionForSlot(int posX, int posY) {
+        return new Vector3(posX * pieceWidth, posY*pieceHeight);
+    }
+    private static Vector3 GetGridSnappedPosition(Vector3 objLocalPosition)
+    {
+        Debug.Log("Math.Floor(" + objLocalPosition.x + " / " + pieceWidth + ")[" + (float)Math.Floor(objLocalPosition.x / pieceWidth) + "]" + " * " + pieceWidth + " = " + ((float)Math.Floor(objLocalPosition.x / pieceWidth) * pieceWidth));
+        return new Vector3(
+            ((float)Math.Round(objLocalPosition.x / pieceWidth) * pieceWidth),
+            ((float)Math.Round(objLocalPosition.y / pieceHeight) * pieceHeight),
+            0
+            );
+    }
 }
--- Assets/Source/Piece.cs
+++ Assets/Source/Piece.cs
@@ -7,6 +7,11 @@ public class Piece : MonoBehaviour {
     private Vector3 screenPoint;
     private Vector3 offset;
     private Sprite mySprite;
+
+    RectTransform rectTransform;
+    public int currentGridX = -1;
+    public int currentGridY = -1;
+
     void Start()
     {
         
@@ -16,34 +21,41 @@ public class Piece : MonoBehaviour {
 
     public void SetPieceImage(Sprite newSprite, int i, int j)
     {
-        var rectTransform = GetComponent<RectTransform>();
+        currentGridX = i;
+        currentGridY = j;
+        rectTransform = GetComponent<RectTransform>();
         if (rectTransform == null) Debug.LogError("where is the rect?");
 
         mySprite = newSprite;
 
         var sr = gameObject.GetComponent<Image>();
         sr.sprite = newSprite;
-        rectTransform.sizeDelta = new Vector2(newSprite.bounds.size.x*100, newSprite.bounds.size.y*100);
+        rectTransform.sizeDelta = new Vector2(newSprite.bounds.size.x, newSprite.bounds.size.y);
         transform.position = new Vector3(i*rectTransform.sizeDelta.x, j*rectTransform.sizeDelta.y, 0);
+        Main.SetValidPosition(this);
     }
-
-    
-
-
-    void Update(){
-
-    }
-    
+   
     public void UI_OnMouseDown(){
         screenPoint = Main.Camera.WorldToScreenPoint(transform.position);
         offset = transform.position - Main.Camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        Main.SelectedPiece = this;
     }
 
     public void UI_OnMouseDrag(){
+        //Debug.Log("mouse "+Input.mousePosition);
         Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         Vector3 curPosition = Main.Camera.ScreenToWorldPoint(curScreenPoint) + offset;
         transform.position = curPosition;
+
+        //Debug.Log(rectTransform.localPosition + " dragging at position " + transform.position);
+
     }
 
-    
+    public void UI_OnMouseUp()
+    {
+        Main.SetValidPosition(this);
+        Main.SelectedPiece = null;
+    }
+
+
 }
